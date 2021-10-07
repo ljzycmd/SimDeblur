9e5ff90ac0dbd5627efd3e66d61de8d2f61ac099
cmd <mingdengcao@gmail.com>
2021-10-07 14:48:17 +0800

    Update author's information

--- setup.py
+++ setup.py
@@ -1,12 +1,14 @@
 from setuptools import setup, find_packages
 
+VERSION = "0.1.0"
+
 setup(
     name="simdeblur",
-    version="0.1",
+    version=VERSION,
     description="A simple deblurring framework for image/video deblurring tasks.",
-    author="CaoMingdeng",
-    author_email="caomingdeng@outlook.com",
-    url="https://github.com/ljzycmd",
+    author="Mingdeng Cao",
+    author_email="mingdengcao@gmail.com",
+    url="https://github.com/ljzycmd/SimDeblur",
     license="MIT",
     packages=find_packages(),
     zip_safe=True
