dd2f60c41176b75c4eaf80d740f547c206aa8227
cmd <mingdengcao@gmail.com>
2022-01-03 22:01:49 +0800

    Update the train and test pipeline.

--- train.py
+++ train.py
@@ -8,9 +8,12 @@
 ************************************************ """
 
 
+import os
+import time
 from simdeblur.config import build_config, merge_args
 from simdeblur.engine.parse_arguments import parse_arguments
 from simdeblur.engine.trainer import Trainer
+from simdeblur.config import save_configs_to_yaml
 
 
 def main():
@@ -19,6 +22,12 @@ def main():
     cfg = build_config(args.config_file)
     cfg = merge_args(cfg, args)
     cfg.args = args
+    cfg.experiment_time = time.strftime("%Y%m%d_%H%M%S")
+    if args.local_rank == 0:
+        save_path = os.path.join(cfg.work_dir, cfg.name, cfg.experiment_time)
+        if not os.path.exists(save_path):
+            os.makedirs(save_path)
+        save_configs_to_yaml(cfg, os.path.join(save_path, cfg.name+".yaml"))
 
     trainer = Trainer(cfg)
     trainer.train()
